c50d276c7ceab3164c942afc7515af10ffd48a11
PDimitroww <plamen.vn.96@gmail.com>
2022-10-06 17:19:15 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// UpperCase bcs its a veriable which is never going to change
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7ee46b38-536c-4abd-ad53-b923fefbcb45';
+export const MODAL_CLOSE_SEC = 2.5;
