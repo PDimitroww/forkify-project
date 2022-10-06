0b3286c16e1c8c648fe7c20531a40fa000e534de
PDimitroww <plamen.vn.96@gmail.com>
2022-10-06 17:57:30 +0300

    fix

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import fracty from 'fracty';
-import View from './view';
+import view from './view';
 
 // import icons from '../img/icons.svg' // Parcel 1
 import icons from 'url:../../img/icons.svg'; //Parcel 2
